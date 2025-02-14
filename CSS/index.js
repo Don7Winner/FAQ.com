36ea51065c3571bd14f35ff49543857e3f61e133
Don7Winner <chidubemweb@gmail.com>
2025-02-14 23:26:52 +0100

    made adjustment to my js folder

--- CSS/index.js
+++ CSS/index.js
@@ -1,47 +1,19 @@
-// THE STARTING OF MY CODE
-$(".sign--1").on("click", function () {
-  $(".toggle-1").toggle();
-});
-document.querySelector(".sign--1").addEventListener("click", function () {
-  document
-    .querySelector(".sign--1")
-    .setAttribute("src", "assets/images/icon-plus.svg");
-  document.querySelector(".sign--1").addEventListener("click", function () {
-    document
-      .querySelector(".sign--1")
-      .setAttribute("src", "assets/images/icon-minus.svg");
-    document.querySelector(".sign--1").addEventListener("click", function () {
-      document
-        .querySelector(".sign--1")
-        .setAttribute("src", "assets/images/icon-plus.svg");
-      document.querySelector(".sign--1").addEventListener("click", function () {
-        document
-          .querySelector(".sign--1")
-          .setAttribute("src", "assets/images/icon-minus.svg");
-        document
-          .querySelector(".sign--1")
-          .addEventListener("click", function () {
-            document
-              .querySelector(".sign--1")
-              .setAttribute("src", "assets/images/icon-plus.svg");
-          });
-      });
-    });
+$(document).ready(function () {
+  $(".FAQ p").hide();
+
+  $(".sign").attr("src", "assets/images/icon-plus.svg");
+  $(".FAQ p").first().show();
+  $(".sign").first().attr("src", "assets/images/icon-minus.svg");
+  $(".minus").click(function () {
+    let answer = $(this).next("p");
+    let icon = $(this).find(".sign");
+
+    answer.slideToggle();
+
+    if (icon.attr("src") === "assets/images/icon-plus.svg") {
+      icon.attr("src", "assets/images/icon-minus.svg");
+    } else {
+      icon.attr("src", "assets/images/icon-plus.svg");
+    }
   });
-});
-
-// THE STARTING OF THE MINU PART
-$(".toggle-2").hide();
-$(".sign--2").on("click", function () {
-  $(".toggle-2").toggle();
-});
-
-$(".toggle--3").hide();
-$(".sign--3").on("click", function () {
-  $(".toggle--3").toggle();
-});
-
-$(".toggle--4").hide();
-$(".sign--4").on("click", function () {
-  $(".toggle--4").toggle();
 });
